6d52b41f2e6c7d6cbe90fc1d466605b79fd25ffa
Hannah Gordon <hannahgordon@sky.com>
2023-05-23 20:38:53 +0100

    Added function to run process and test

--- get_data/tasks.py
+++ get_data/tasks.py
@@ -3,11 +3,11 @@ from pyot.core.queue import Queue
 from google.cloud import pubsub_v1
 from google.cloud import storage
 from datetime import timedelta, datetime
-
 import asyncio
 
 
-async def consume_summoner(summoner, start_ts, end_ts, summoner_output_func=print, match_output_func=None):
+async def consume_summoner(summoner, start_ts, end_ts, summoner_output_func=print, match_output_func=None,
+                           summoner_output_location=None, match_output_location=None):
     await summoner.get()
     print("getting data for {}".format(summoner.name))
     summoner_data = summoner.raw()
@@ -17,21 +17,30 @@ async def consume_summoner(summoner, start_ts, end_ts, summoner_output_func=prin
     summoner_data['match_history'] = history.raw()
 
     if summoner_output_func is not None:
-        summoner_output_func(summoner_data)
+        if summoner_output_location is None:
+            summoner_output_func_args = []
+        else:
+            summoner_output_func_args = [summoner_output_location, "{}_{}_{}.json".format(summoner.puuid, str(start_ts), str(end_ts))]
+        summoner_output_func(str(summoner_data), *summoner_output_func_args)
 
     if match_output_func is not None:
         for match in history.matches:
-            await consume_match(match, match_output_func)
+            if match_output_location is None:
+                match_output_func_args = []
+            else:
+                match_output_func_args = [match_output_location, "{}.json".format(match.id)]
+            await consume_match(match, match_output_func, match_output_func_args)
 
     return summoner_data
 
 
-async def get_summoner_matches(puuids, start_ts, end_ts, topic_name):
-    publisher = pubsub_v1.PublisherClient()
+async def get_summoner_matches(puuids, start_ts, end_ts, summoner_output_func, match_output_func,
+                               summoner_output_func_args=[], match_output_func_args=[]):
     async with Queue() as queue:
         for puuid in puuids:
             summoner = tft.Summoner(puuid=puuid)
-            await queue.put(consume_summoner(summoner, start_ts, end_ts, topic_name, publisher))
+            await queue.put(consume_summoner(summoner, start_ts, end_ts, summoner_output_func, match_output_func,
+                                             summoner_output_func_args, match_output_func_args))
         return await queue.join()
 
 
@@ -51,13 +60,48 @@ def gcs_read(bucket_name, blob_name):
         return f.read()
 
 
-async def consume_match(match, output_func):
+def gcs_write(content, bucket_name, blob_name):
+    """Write and read a blob from GCS using file-like IO"""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your new GCS object
+    # blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    with blob.open("w") as f:
+        f.write(content)
+
+
+async def consume_match(match, output_func, output_func_args=[]):
     await match.get()
     print("{} : {}".format(match.id, match.info.datetime))
-    return output_func(str(match.raw()))
+    return output_func(str(match.raw()), *output_func_args)
 
 
 def get_time_range(run_scheduled, time_window_hours, time_offset_hours):
     end_ts = run_scheduled - timedelta(hours=time_offset_hours)
     start_ts = end_ts - timedelta(hours=time_window_hours)
     return start_ts, end_ts
+
+
+async def process_run(run_scheduled, config_dict):
+    start_time, end_time = get_time_range(run_scheduled, config_dict['time_window_hours'],
+                                          config_dict['time_offset_hours'])
+    puuids = gcs_read(config_dict['input_bucket_name'], config_dict['input_blob_name']).split()
+    if config_dict['num_puuids'] is not None:
+        to_use_puuids = puuids[:config_dict['num_puuids']]
+    else:
+        to_use_puuids = puuids
+
+    await get_summoner_matches(to_use_puuids,
+                               start_time,
+                               end_time,
+                               config_dict['summoner_output_func'],
+                               config_dict['match_output_func'],
+                               config_dict['summoner_output_location'],
+                               config_dict['match_output_location']
+                               )
--- tests.py
+++ tests.py
@@ -6,10 +6,11 @@ import io
 import datetime
 from pyot.core.queue import Queue
 import asyncio
+from google.cloud import storage
 import main
 
 
-class MyTestCase(unittest.TestCase):
+class UnitTests(unittest.TestCase):
 
     def setUp(self):
         self.event = type('cloudevent', (object,), {"attributes": {}, "data": {}})
@@ -67,6 +68,69 @@ class PyotTestCase(unittest.IsolatedAsyncioTestCase):
         async with Queue() as queue:
             await tasks.consume_match(self.match, print)
             self.assertIn("'match_id': 'EUW1_6359993696'", mock_stdout.getvalue())
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    async def test_get_summoner_matches(self, mock_stdout):
+        test_starttime, test_endtime = datetime.datetime(2023, 4, 11, 11, 0), datetime.datetime(2023, 4, 11, 23, 0)
+        test_puuids = ['IZnpt2d5pvurXXIOcl9aSFD5eooUnMcvjAM1C8BJ32Snp-EZflUu5pdFwzM-4YoXmLpStAu7Plf2dA',
+                       'IMKXFDgs25rueiGfxVjkkMD-u97oeoUw3NBrLp61SXDrm2ZPN-gcv4qgjZHYxpaL6UiEygvfQpD65A']
+        await tasks.get_summoner_matches(test_puuids, test_starttime, test_endtime, print, None)
+        self.assertIn("spannertft", mock_stdout.getvalue())
+        self.assertIn("SueMG", mock_stdout.getvalue())
+
+
+class ProcessTestCase(unittest.IsolatedAsyncioTestCase):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    def setUp(self):
+        from pyot.conf.utils import import_confs
+        import_confs("get_data.pyot_config")
+        from pyot.models import tft
+        self.summoner = tft.Summoner(name="spannertft")
+        self.match = tft.Match(id="EUW1_6359993696")
+        self.bucket_name = 'tft_test_' + datetime.datetime.now().strftime("%Y_%m_%d_%H%M%S")
+        storage_client = storage.Client(project="verdant-wave-375715")
+        self.bucket = storage_client.create_bucket(self.bucket_name)
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    async def test_process_run(self, mock_stdout):
+        configs = {
+            'project_name': 'verdant-wave-375715',
+            'input_bucket_name': "summoner_checklist",
+            'input_blob_name': "summoner_list_test.csv",
+            'summoner_output_func': tasks.gcs_write,
+            'summoner_output_location': self.bucket_name,
+            'match_output_func': tasks.gcs_write,
+            'match_output_location': self.bucket_name,
+            'num_puuids': 2,
+            'time_window_hours': 12,
+            'time_offset_hours': 1
+        }
+
+        run_scheduled = datetime.datetime(2023, 4, 3, 0, 0, 0)
+
+        await tasks.process_run(run_scheduled, configs)
+
+        expected_files = [
+            "IMKXFDgs25rueiGfxVjkkMD-u97oeoUw3NBrLp61SXDrm2ZPN-gcv4qgjZHYxpaL6UiEygvfQpD65A_2023-04-02 11:00:00_2023-04-02 23:00:00.json",
+            "IZnpt2d5pvurXXIOcl9aSFD5eooUnMcvjAM1C8BJ32Snp-EZflUu5pdFwzM-4YoXmLpStAu7Plf2dA_2023-04-02 11:00:00_2023-04-02 23:00:00.json",
+            "EUW1_6343076097.json",
+            "EUW1_6343131079.json",
+            "EUW1_6343372556.json",
+            "EUW1_6343653546.json",
+            "EUW1_6343872631.json",
+            "EUW1_6343995009.json"
+            ]
+        expected_files.sort()
+
+        resulting_files = [blob.name for blob in self.bucket.list_blobs()]
+        resulting_files.sort()
+        self.assertListEqual(expected_files, resulting_files)
+
+    def tearDown(self):
+        for blob in self.bucket.list_blobs():
+            blob.delete()
+        self.bucket.delete()
 
 
 if __name__ == '__main__':
